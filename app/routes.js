ff3122e6ff1e19236d8d7f38b26ac84823b6e9b2
Rigoberto <rresendez@rigoresendez.com>
2017-03-22 16:27:34 -0500

    Real data update

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,8 @@ module.exports = function(app, passport) {
 	var fs = require('fs');
 	var csv = require('fast-csv');
 	var mysql = require("mysql");
+	var dialog = require('dialog');
+	const csv_a="c_staffname,c_office_name,d_ap_date,t_ap_starttime,t_ap_endtime,c_ap_confirm,c_pe_patient_id,c_pe_name,n_pe_age,c_ap_code,c_ap_desc,c_pe_wphone,c_pe_hphone,c_pe_w_ext,c_ref_name,c_ap_note,c_pe_chart,n_bl_id,d_pe_dob,m_pe_ins_due,m_pe_pat_due,n_pe_id,name_1,c_apc_id,c_user_id,c_vrc_desc,l_apt_new_patient,c_st_id";
 	var keys = require('./keys');
   var passw = keys.pass;
 
@@ -19,9 +21,13 @@ module.exports = function(app, passport) {
 	filename: function (req, file, cb) {
 		cb(null,'test.csv');
 	}
+
+
 });
 // initialize upload object with multer for single file
+	var uploadB = multer({ storage: multer.memoryStorage({}) }).single('csv');
 	var upload = multer({storage: storage}).single('csv');
+
 
 
 
@@ -88,6 +94,25 @@ app.get('/upload',isLoggedIn, function(req, res){
 
 // show result from input
 app.post('/upload',isLoggedIn, function(req,res){
+//Check buffer to ensure csv format match
+
+	uploadB(req,res, function (err){
+		if(err){
+			console.log(err);
+		}
+
+		 console.log("Buffer");
+		 var buff = req.file.buffer.toString('utf-8',0,312)
+     if(csv_a!=buff){
+			 dialog.warn("The file you uploaded, does not match the csv format, please try again.");
+			 res.redirect('/upload');
+
+		 }
+
+	})
+// If the format matches go ahead an upload the file
+
+
 	upload(req,res, function (err){
 		if(err){
 			console.log(err);
@@ -115,7 +140,7 @@ app.post('/sql',isLoggedIn, function(req, res){
 	// Validation
 
 	  .on("data", function(data){
-			console.log(data);
+
 //MYSQL_CONNECTION=======================
 
     //MySQL conneciton to db
@@ -136,6 +161,7 @@ app.post('/sql',isLoggedIn, function(req, res){
       }
       console.log('Connection to MySQL established');
     });
+/**********THIS ARE THE OLD QUERIES
 
     //Creating query
     var qry_cnt = "SELECT count(1) AS num,count(statecode) AS state from csv where policyID=";
@@ -180,10 +206,58 @@ app.post('/sql',isLoggedIn, function(req, res){
 
 		})
 	})
+	*/
 
 
 		//CSV_END====================================
+		//Get ID from tbl_patient
 
+		if(data.c_pe_patient_id!=""){
+		console.log("Patient id: "+data.c_pe_patient_id);
+		con.query("SELECT * FROM tbl_patient WHERE numid_patient=?",[data.c_pe_patient_id], function(err,result){
+			if(err) throw err;
+			if(result.length>0){
+			console.log("Real id: "+result[0].id);
+			var real_id=result[0].id;
+
+			//Begin second query on tbl_patient
+			//Split csv date into date and time
+			var temp_data = data.t_ap_starttime.split(" ");
+			var data_date = temp_data[0];
+			//Check for wrong time format
+			if(temp_data[1][1]==":"){
+				temp_data[1]="0"+temp_data[1];
+			}
+			//Finish correcting wrong time format 
+			var data_time = temp_data[1]+":00";
+			// Testing for correct splitashion XD
+			console.log("Date: "+ data_date + " Time: "+ data_time);
+			//Second query
+			con.query("SELECT * FROM tbl_consult WHERE id_patient=? and date_consult=?",[real_id,data_date],function (err_b,result_b){
+			if(err_b) throw err_b;
+			//Loop trough results
+			for(var i =0; i<result_b.length;i++){
+			console.log("Result date from 2nd Query:"+ result_b[i].end_consult);
+			//Check state for row completion
+			if(result_b[i].id_state!=4){
+			//Check for no time match in DB
+				if(result_b[i].end_consult!=data_time){
+					console.log("Time from DB does not match CSV");
+				}
+				else{
+					console.log("Time from DB matches CSV");
+				}
+
+			}
+		}
+
+
+			})
+		}
+		})
+	}
+
+	})
 
 
 		.on("end", function(){
