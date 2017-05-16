56bab4115a826e9e8c4103fb7392fe92d9b744a6
Rigoberto <rresendez@rigoresendez.com>
2017-05-16 10:57:32 -0500

    Asthethics

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': "192.168.1.15",
+        'host': "192.168.10.15",
         'user': 'practez_modules',
         'password': 'IngPss2015$@',
         'database': 'test_db_BETA02'
