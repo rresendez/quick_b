feaf840359a95e0ea1914005b189917900a11d66
Rigoberto <rresendez@rigoresendez.com>
2017-05-26 10:54:54 -0500

    State 5 bug for linux

---
+++ config/database_aws.js
@@ -0,0 +1,16 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': "inartec-db1.caqs6gipj1jl.sa-east-1.rds.amazonaws.com",
+        'user': 'swe',
+        'password': 'ingenium2015',
+        'database': 'it01_db_beta01e_medicalpractice',
+        'connectionTimeout':1000000,
+        'connectionLimit':10000000,
+        'queueLimit':10000000
+    },
+	'database': 'it01_db_beta01e_medicalpractice',
+    'users_table': 'tbl_users_servs',
+    'log_table': 'logs'
+
+};
